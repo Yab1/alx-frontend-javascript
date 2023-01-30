7a5736ab4339f8d23a7251ab3e6ab36b8b2b3074
Yab1 <yabilisanu@gmail.com>
2023-01-30 19:21:13 +0300

    Combine

---
+++ 0x03. ES6 data manipulation/4-update_grade_by_city.js
@@ -0,0 +1,22 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  // filter students by city
+  const filteredStudents = students.filter(
+    (student) => student.location === city
+  )
+  // added grade property on the students list
+  newGrades.map((newGrade) => {
+    for (const studentObj of filteredStudents) {
+      if (studentObj.id === newGrade.studentId) {
+        studentObj['grade'] = newGrade.grade
+      }
+    }
+  })
+  // If a student doesn’t have grade in newGrades, the final grade should be N/A
+  filteredStudents.map((studentsListGrade) => {
+    if (!studentsListGrade.grade) {
+      studentsListGrade['grade'] = 'N/A'
+    }
+  })
+
+  return filteredStudents
+}
