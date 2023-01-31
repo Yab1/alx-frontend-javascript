f0b7c974702699b86127c79c8a519c62272872c4
Yab1 <yabilisanu@gmail.com>
2023-01-31 13:58:56 +0300

    deleted comments

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,3 @@
 export default function getListStudentIds(input) {
-  // solution One
-  //   if (typeof input === 'object') {
-  //     const arrObjIndex = Object.keys(input)
-  //     return arrObjIndex.map((ele) => input[ele].id)
-  //   } else return []
-  //updated solution
   return Array.isArray(input) ? input.map((ele) => ele.id) : []
 }
