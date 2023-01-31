f087b735b6579e24c652e7c27dc54dd7c31987ee
Yab1 <yabilisanu@gmail.com>
2023-01-31 15:08:12 +0300

    find or filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const filteredStudents = students.filter(
-    (student) => student.location === city
-  );
-  newGrades.map((newGrade) => {
-    for (const studentObj of filteredStudents) {
-      if (studentObj.id === newGrade.studentId) {
-        studentObj['grade'] = newGrade.grade;
-      }
-    }
-  });
-  filteredStudents.map((studentsListGrade) => {
-    if (!studentsListGrade.grade) {
-      studentsListGrade['grade'] = 'N/A';
-    }
-  });
-  return filteredStudents;
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const currentStudGrade = newGrades.filter(
+        (newGrade) => student.id === newGrade.studentId
+      )[0];
+      return currentStudGrade
+        ? { ...student, grade: currentStudGrade.grade }
+        : { ...student, grade: 'N/A' };
+    });
 }
