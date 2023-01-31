1cab7b37d7d4bcfeb2059194c4048c884c3ba2b4
Yab1 <yabilisanu@gmail.com>
2023-01-31 17:52:46 +0300

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,7 @@
-export default function createInt8TypedArray(length, position, value) {}
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length); // ArrayBuffer
+  const dataView = new DataView(buffer); // DataView
+  if (position >= length) throw new RangeError('Position outside range'); // Throws RangeError if it exceeds length
+  dataView.setUint8(position, value);
+  return dataView;
+}
