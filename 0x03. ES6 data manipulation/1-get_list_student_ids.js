e73b778ac677a2baa6f25319a81a0ee12cead2e9
Yab1 <yabilisanu@gmail.com>
2023-01-31 13:40:01 +0300

    deleted 0x03

--- 0x03. ES6 data manipulation/1-get_list_student_ids.js
+++
@@ -1,9 +0,0 @@
-export default function getListStudentIds(input) {
-  // solution One
-  //   if (typeof input === 'object') {
-  //     const arrObjIndex = Object.keys(input)
-  //     return arrObjIndex.map((ele) => input[ele].id)
-  //   } else return []
-  //updated solution
-  return Array.isArray(input) ? input.map((ele) => ele.id) : []
-}
--- 0x03. ES6 data manipulation/2-get_students_by_loc.js
+++
@@ -1,3 +0,0 @@
-export default function getStudentIdsSum(students, city) {
-  return students.filter((student) => student.location === city)
-}
